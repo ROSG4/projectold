3b26e8d08308856574c450633cc5ae99df91ab68
ROSG4 <168713749+ROSG4@users.noreply.github.com>
2024-05-14 18:08:53 +0800

    Add files via upload

--- ros_workspace/src/gamepad/launch/joy.launch.py
+++ ros_workspace/src/gamepad/launch/joy.launch.py
@@ -15,14 +15,14 @@ def generate_launch_description():
     joy_node = Node(
             package='joy',
             executable='joy_node',
-            parameters=[joy_params],
+            parameters=[joy_params, {use_sim_time: 'use_sim_time'}],
          )
 
     teleop_node = Node(
             package='teleop_twist_joy',
             executable='teleop_node',
             name='teleop_twist_node',
-            parameters=[joy_params],
+            parameters=[joy_params, {use_sim_time: 'use_sim_time'}],
             # remappings=[('/cmd_vel','/cmd_vel_joy')]
          )
 
